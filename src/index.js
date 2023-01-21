a84946e70a132dfdaf3c5f4f02bf02a78fa02a93
Ionut <35095884+IonutCioarec@users.noreply.github.com>
2023-01-21 02:25:55 +0200

    Added the multiverseX modules

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ProSidebarProvider } from "react-pro-sidebar";
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,7 +10,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <ProSidebarProvider>
+        <App />
+      </ProSidebarProvider>
   </React.StrictMode>
 );
 
