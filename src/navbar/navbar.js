9f1e65bd91295674886ecd672da7cad230392e32
Ionut <35095884+IonutCioarec@users.noreply.github.com>
2023-02-03 01:20:05 +0200

    Added the menu and login to the navbar

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useGetAccountInfo } from "@multiversx/sdk-dapp/hooks/account";
-import { logout, getIsLoggedIn } from '@multiversx/sdk-dapp/utils';
+import { logout } from '@multiversx/sdk-dapp/utils';
 import { useGetPendingTransactions} from '@multiversx/sdk-dapp/hooks/transactions';
 import { ExtensionLoginButton, WalletConnectLoginButton, LedgerLoginButton, WebWalletLoginButton} from "@multiversx/sdk-dapp/UI";
 import { useProSidebar } from "react-pro-sidebar";
@@ -8,60 +8,179 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
-import { BarChart } from 'assets/svg/BarChart';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-
 import {Account} from "utils/multiversX";
+
 
 export function Navbar() {
     const { toggleSidebar, broken } = useProSidebar();
+    const [timeToConnect, setTimeToConnect] = React.useState(false);
 
-    let connectSection =
+    let connectSection = timeToConnect ? (
         <Container>
-            <Row>
-                <Col xs={12} lg={{offset: 4, span: 4}}>
+            <Row className="justify-content-start">
+                <Col xs={12} lg={{offset: 4, span: 4}} className="text-center">
                     <div className="farm-card" >
-                        <Button className="float-end">
-                            sdsd
-                        </Button>
+                        <Row>
+                            <Col>
+                                <Button
+                                    variant="light" size="sm"
+                                    className="btn btn-light float-end"
+                                    style={{width: '30px'}}
+                                    onClick={() =>  setTimeToConnect(false)}
+                                >
+                                    <FontAwesomeIcon icon="fa-xmark" />
+                                </Button>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <p className="h6 text-white font-size-lg">Connect to a wallet</p>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="text-center">
+                                <WebWalletLoginButton
+                                    callbackRoute="/"
+                                    loginButtonText={"Web wallet"}
+                                    className="btn btn-sm dapp-primary font-size-sm w-60"
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <LedgerLoginButton
+                                    loginButtonText={"Ledger"}
+                                    callbackRoute="/"
+                                    className="btn btn-sm dapp-primary font-size-sm w-60"
+                                />
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <WalletConnectLoginButton
+                                    callbackRoute="/"
+                                    loginButtonText={"Maiar"}
+                                    className="btn btn-sm dapp-primary font-size-sm w-60"
+                                />
+                            </Col>
+                        </Row>
+                        <Row className="mb-4">
+                            <Col>
+                                <ExtensionLoginButton
+                                    callbackRoute="/"
+                                    loginButtonText={"Extension"}
+                                    className="btn btn-block btn-sm dapp-primary font-size-sm w-60 mt-1"
+                                />
+                            </Col>
+                        </Row>
                     </div>
                 </Col>
             </Row>
         </Container>
+        ): (
+            ''
+        )
     ;
 
     if(Account().isLoggedIn){
-        return ('');
+        return (
+            broken? (
+                <React.Fragment>
+                    <Row>p
+                        <Col xs={6}>
+                            <Button
+                                variant="outline-light"
+                                size="sm"
+                                className="btn btn-block"
+                                onClick={() => toggleSidebar()}
+                            >
+                                <FontAwesomeIcon icon="fa-bars" style={{marginRight: '5px'}} size={"xs"}/>
+                                Menu
+                            </Button>
+                        </Col>
+                        <Col xs={6}>
+                            <Button
+                                variant="outline-light"
+                                size="sm"
+                                className="btn btn-block"
+                            >
+                                <FontAwesomeIcon icon="fa-user" style={{marginRight: '5px'}} size={"xs"}/>
+                                My Account
+                            </Button>
+                        </Col>
+                        {connectSection}
+                    </Row>
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <Container>
+                        <Row>
+                            <Col lg={{offset: 10, span: 2}}>
+                                <Button
+                                    variant="outline-light"
+                                    size="sm"
+                                    className="btn btn-block"
+                                >
+                                    <FontAwesomeIcon icon="fa-user" style={{marginRight: '5px'}} size={"xs"}/>
+                                    My Account
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                    {connectSection}
+                </React.Fragment>
+            )
+        );
     }else{
         return (
             broken? (
-                <Row>
-                    <Col xs={6}>
-                        <Button variant="outline-light" size="sm" className="btn btn-block" onClick={() => toggleSidebar()}>
-                            <FontAwesomeIcon icon="fa-wallet" style={{marginRight: '5px'}} size={"sm"}/>
-                            Menu
-                        </Button>
-                    </Col>
-                    <Col xs={6}>
-                        <Button variant="outline-light" size="sm" className="btn btn-block" onClick={() => toggleSidebar()}>
-                            <FontAwesomeIcon icon="fa-wallet" style={{marginRight: '5px'}} size={"sm"}/>
-                            Connect
-                        </Button>
-                    </Col>
-                    {connectSection}
-                </Row>
-            ) : (
-                <Container>
+                <React.Fragment>
                     <Row>
-                        <Col lg={{offset: 10, span: 2}}>
-                            <Button variant="outline-light" size="sm" className="btn btn-block" onClick={() => toggleSidebar()}>
-                                <FontAwesomeIcon icon="fa-wallet" style={{marginRight: '5px'}} size={"sm"}/>
+                        <Col xs={6}>
+                            <Button
+                                variant="outline-light"
+                                size="sm"
+                                className="btn btn-block"
+                                onClick={() => toggleSidebar()}
+                            >
+                                <FontAwesomeIcon icon="fa-bars" style={{marginRight: '5px'}} size={"xs"}/>
+                                Menu
+                            </Button>
+                        </Col>
+                        <Col xs={6}>
+                            <Button
+                                variant="outline-light"
+                                size="sm"
+                                className="btn btn-block"
+                                onClick={() =>  setTimeToConnect(prevCheck => !prevCheck)}
+                            >
+                                <FontAwesomeIcon icon="fa-wallet" style={{marginRight: '5px'}} size={"xs"}/>
                                 Connect
                             </Button>
                         </Col>
+                        {connectSection}
                     </Row>
+                </React.Fragment>
+            ) : (
+                <React.Fragment>
+                    <Container>
+                        <Row>
+                            <Col lg={{offset: 10, span: 2}}>
+                                <Button
+                                    variant="outline-light"
+                                    size="sm"
+                                    className="btn btn-block"
+                                    onClick={() =>  setTimeToConnect(prevCheck => !prevCheck)}
+                                >
+                                    <FontAwesomeIcon icon="fa-wallet" style={{marginRight: '5px'}} size={"xs"}/>
+                                    Connect
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Container>
                     {connectSection}
-                </Container>
+                </React.Fragment>
             )
         );
     }
--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,6 +1,3 @@
-import { useGetAccountInfo } from "@multiversx/sdk-dapp/hooks/account";
-
-
 //Export the environment const for Dapp Provider
 // export const environment = 'devnet';
 // export const environment = 'testnet';
