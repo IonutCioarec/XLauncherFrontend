57e10ca80100adb92e7c6001e88b7f8a02384ac2
Ionut <35095884+IonutCioarec@users.noreply.github.com>
2023-03-17 02:48:49 +0200

    Added gasFeeLimit + intlNumberFormat functions

--- src/utils/utilities.js
+++ src/utils/utilities.js
@@ -38,3 +38,33 @@ export const convertWeiToEsdt = (amount, decimals, precision) => {
         return BigNumber(amount).shiftedBy(typeof(decimals) !== 'undefined' ? -decimals : -18).decimalPlaces(typeof(precision) !== 'undefined' ? precision : 4, BigNumber.ROUND_DOWN);
     }
 };
+
+//Calculate the gas fee limit
+export function calculateGasLimit(clientStateData) {
+    const countItems = Object.keys(clientStateData).length;
+    const n = countItems * (countItems + 1) / 2;
+    const l = 80000 * (10 * countItems + n) + 10000000 + (1000000 * countItems);
+    let gasLimit = 10000000;
+
+    if (l >= 10000000 && l < 30000000) {
+        gasLimit = 50000000;
+    } else if (l >= 20000000 && l < 30000000) {
+        gasLimit = 70000000;
+    } else if (l >= 30000000 && l < 60000000) {
+        gasLimit = 90000000;
+    } else if (l >= 60000000 && l < 100000000) {
+        gasLimit = 200000000;
+    } else if (l >= 100000000) {
+        gasLimit = 600000000;
+    }
+
+    return gasLimit;
+}
+
+//Format number
+export function intlNumberFormat(number, locales, minDigits, maxDigits){
+    return new Intl.NumberFormat(locales? locales : "en-GB", {
+        minimumFractionDigits: minDigits? minDigits : 2,
+        maximumFractionDigits: maxDigits? maxDigits : 2,
+    }).format(number);
+}
