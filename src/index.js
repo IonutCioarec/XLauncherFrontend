b118f538b06a9aea849b8bff6906a9f144c853cb
Ionut <35095884+IonutCioarec@users.noreply.github.com>
2023-03-17 02:50:29 +0200

    Added plus-jakarta-sans font

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { ProSidebarProvider } from "react-pro-sidebar";
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "@fontsource/plus-jakarta-sans";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
